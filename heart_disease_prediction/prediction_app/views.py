c11fa26d1c17016f300e1167ff9af0abeb9749da
cindrella koju <cindrellakoju@gmail.com>
2025-05-18 19:24:07 +0545

    html page rendered

--- heart_disease_prediction/prediction_app/views.py
+++ heart_disease_prediction/prediction_app/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    context = {
+        'name' : "Cindrella Koju"
+    }
+    return render(request,'homepage.html',context)
+    # return HttpResponse("Hello from prediction App")
